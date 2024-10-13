05f73c9116009ee05f8a1114054bf8b7a41b54d5
Krasimir Kanev <71128514+knaevKMK@users.noreply.github.com>
2024-10-13 22:26:56 +0300

    ADD: restore team

---
+++ Footbal.League.Application/Footbal.League.API/src/Application/BoundedContexts/FootballTeams/Commands/RestoreFootballTeamCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.BoundedContexts.FootballTeams.Commands
+{
+
+    using Application.Common.Contracts;
+    using Application.Common;
+    using Domain.BoundedContexts.FootbalTeam.Repositories;
+    using MediatR;
+
+    public class RestoreFootballTeamCommand : IRequest<Result<bool>>
+    {
+        public Guid TeamId { get; set; }
+        public class RestoreFootballTeamHandler(
+            ICurrentUser currentUser,
+            IFootballTeamsDomainRepository teamRepository
+            ) : IRequestHandler<RestoreFootballTeamCommand, Result<bool>>
+        {
+            public async Task<Result<bool>> Handle(RestoreFootballTeamCommand request, CancellationToken cancellationToken)
+            {
+                var teamEntity = await teamRepository.FindTeamByIdAsync(request.TeamId, cancellationToken);
+
+                teamEntity.Restore(currentUser.UserIdAsGuid());
+
+                await teamRepository.UpdataTeamAsync(teamEntity);
+
+                return true;
+            }
+        }
+    }
+}
