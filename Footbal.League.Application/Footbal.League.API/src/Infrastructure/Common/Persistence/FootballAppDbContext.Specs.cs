bf24aba29d761ab9379dad2915d0b29d5700677d
Krasimir Kanev <71128514+knaevKMK@users.noreply.github.com>
2024-10-14 10:07:59 +0300

    ADD: db context test

---
+++ Footbal.League.Application/Footbal.League.API/src/Infrastructure/Common/Persistence/FootballAppDbContext.Specs.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.BoundedContexts.FootballMatch;
+using Infrastructure.BoundedContexts.FootballTeam;
+using Infrastructure.Common.Events;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection; 
+using System.Reflection;
+using Xunit;
+
+namespace Infrastructure.Common.Persistence
+{
+    public class FootballAppDbContextSpecs
+    {
+        [Fact]
+        public async Task RaisedEventsShouldBeHandled()
+        {
+            // Arrange
+            var services = new ServiceCollection()
+                .AddDbContext<FootballAppDbContext>(opts => opts
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString()))
+                .AddScoped<IFootballMatchDbContext>(provider => provider
+                    .GetService<FootballAppDbContext>()!)
+                .AddScoped<IFootballTeamDbContext>(provider => provider
+                    .GetService<FootballAppDbContext>()!) 
+                .AddTransient<IEventDispatcher, EventDispatcher>()
+                .AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddRepositories()
+                .BuildServiceProvider();
+        }
+    }
+}
