9f2c0ec9128799c608bc8d2ea9bd042d6b8abb52
Krasimir Kanev <71128514+knaevKMK@users.noreply.github.com>
2024-10-14 10:26:37 +0300

    ADD: entities tests

---
+++ Footbal.League.Application/Footbal.League.API/src/Domain/Common/Models/BaseEntity.Specs.cs
@@ -0,0 +1,81 @@
+﻿namespace  Domain.Common.Models
+{
+    using Bogus;
+    using Domain.BoundedContexts.FootballTeam.Entities;
+    using Domain.Common.Extensions;
+    using Domain.Common.Services;
+    using FluentAssertions;
+    using Xunit;
+     
+
+    public class BaseEntitySpecs : IClassFixture<FakeDateTimeProviderFakes>
+    {
+        private readonly FakeDateTimeProviderFakes _dateTimeProviderFakes;
+
+        public BaseEntitySpecs(FakeDateTimeProviderFakes fakeProvider)
+        {
+            _dateTimeProviderFakes = fakeProvider;
+        }
+
+        [Fact]
+        public void OnCreation_ValidatesConstructor_ShouldBeValid()
+        {
+            var faker = new Faker();
+            DateTime randomDate = faker.Date.Soon();
+
+            _dateTimeProviderFakes.UtcNow = randomDate;
+
+            var model = new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.NewGuid(),
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(Guid.NewGuid()); 
+
+            model.CreatedOn.Should().Be(randomDate);
+        }
+
+        [Fact]
+        public void OnCreation_ValidatesConstructor_ShouldNotBe()
+        {
+            var faker = new Faker();
+            DateTime randomDate = faker.Date.Soon();
+
+            _dateTimeProviderFakes.UtcNow = randomDate;
+
+            // Act
+            var model = new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.NewGuid(),
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(Guid.NewGuid());
+
+            randomDate = randomDate.AddMinutes(1);
+
+            // Assert
+            model.CreatedOn.Should().NotBe(randomDate);
+        }
+
+        [Fact]
+        public void OnEmptyCreatedFrom_ValidatesConstructor_ShouldThrowBaseDomainException()
+        {
+            // Act
+            Action act = () => new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.Empty,
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(Guid.NewGuid());
+
+            // Assert
+            act.Should().Throw<BaseDomainException>();
+        }
+    }
+}
---
+++ Footbal.League.Application/Footbal.League.API/src/Domain/Common/Models/Entity.Specs.cs
@@ -0,0 +1,80 @@
+﻿namespace Domain.Common.Models
+{
+    using Bogus;
+    using Domain.BoundedContexts.FootballTeam.Entities;
+    using Domain.Common.Extensions;
+    using Domain.Common.Services;
+    using FluentAssertions;  
+    using Xunit;
+
+    public class EntitySpecs : IClassFixture<FakeDateTimeProviderFakes>
+    {
+        private readonly FakeDateTimeProviderFakes _dateTimeProviderFakes;
+
+        public EntitySpecs(FakeDateTimeProviderFakes fakeProvider)
+        {
+            _dateTimeProviderFakes = fakeProvider;
+        }
+
+        [Fact]
+        public void EntitiesWithEqualIdsShouldBeEqual()
+        {
+            var id = Guid.NewGuid();
+           
+            var first = new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.NewGuid(),
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(id);
+
+            var second =  new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.NewGuid(),
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(id); ;
+
+            // Act
+            var result = first == second;
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void EntitiesWithDifferentIdsShouldNotBeEqual()
+        {
+            // Arrange
+            var first = new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.NewGuid(),
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(Guid.NewGuid());
+
+            var second = new Faker<FootballTeamEntity>()
+                .CustomInstantiator(f => new FootballTeamEntity(
+                    Guid.NewGuid(),
+                    "Team Name",
+                    f.Lorem.Letter(100),
+                    _dateTimeProviderFakes))
+                .Generate()
+                .SetId(Guid.NewGuid());
+
+            // Act
+            var result = first == second;
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+
+
+}
